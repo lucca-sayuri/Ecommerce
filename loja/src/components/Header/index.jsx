a8329b23fed3761e43e10651dd78b3e743d8424e
lucca-sayuri <kelcoolg@gmail.com>
2026-03-10 07:19:52 -0300

    Commit inicial

---
+++ loja/src/components/Header/index.jsx
@@ -0,0 +1,26 @@
+import './Header.css'
+
+export default function Header() {
+    return (
+        <header>
+            <div className='anuncio'>
+                <p>Tudo em 10x sem juros!</p>
+            </div>
+
+            <div className='header'>
+                <picture>
+                    <a href="#">
+                        <img src="./logo-flor.png"/>
+                    </a>
+                </picture>
+
+                <nav>
+                    <a href="#">Home</a>
+                    <a href="#">Produtos</a>
+                    <a href="#">Lançamentos</a>
+                    <a href="#">Contato</a>
+                </nav>
+            </div>
+        </header>
+    )
+}

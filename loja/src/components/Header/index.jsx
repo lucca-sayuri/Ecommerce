9e09c0f3d0342ac18ddfd112ebbd2486b9e1adbd
Lucca Yuki <kelcoolg@gmail.com>
2026-03-17 08:29:43 -0300

    Header

--- loja/src/components/Header/index.jsx
+++ loja/src/components/Header/index.jsx
@@ -4,7 +4,9 @@ export default function Header() {
     return (
         <header>
             <div className='anuncio'>
-                <p>Tudo em 10x sem juros!</p>
+                <div className='anuncio-conteudo'>
+                    <p>Tudo em 10x sem juros!</p>
+                </div>
             </div>
 
             <div className='header'>
